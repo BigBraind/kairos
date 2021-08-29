1680da757479bebb7f8d825415d2c3d6516cadb8
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2021-08-29 10:09:32 +0800

    Comma

--- chat/config/prod.exs
+++ chat/config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :chat, ChatWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["//localhost", "//*.bigbrain.link"]
+  check_origin: ["//localhost", "//*.bigbrain.link"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
