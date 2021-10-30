a7f2387fa6d47f5fcac2fc47144759d715299a6a
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2021-10-30 10:15:30 +0800

    Welcome Test Case

--- lifecycle/test/lifecycle_web/controllers/page_controller_test.exs
+++ lifecycle/test/lifecycle_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LifecycleWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Bigbrain!"
   end
 end
