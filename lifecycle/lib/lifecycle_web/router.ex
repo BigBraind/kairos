c9306856ffff6fc613a1ecb22b99f8c1613238ee
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2021-12-30 22:12:58 +0800

    :new, :edit ACTION handling on SHOW

--- lifecycle/lib/lifecycle_web/router.ex
+++ lifecycle/lib/lifecycle_web/router.ex
@@ -36,6 +36,7 @@ defmodule LifecycleWeb.Router do
 
     live "/phases/:id", PhaseLive.Show, :show
     live "/phases/:id/show/edit", PhaseLive.Show, :edit
+    live "/phases/:id/show/new", PhaseLive.Show, :new
   end
 
   # Other scopes may use custom stacks.
