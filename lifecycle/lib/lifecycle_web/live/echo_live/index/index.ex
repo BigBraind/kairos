5793200b3696b9482fc3caf95dd8d4af2817d966
huSKY <jetxuen_lee@mymail.sutd.edu.sg>
2022-01-08 22:11:55 +0800

    restricted users approve transition multiple times

--- lifecycle/lib/lifecycle_web/live/echo_live/index/index.ex
+++ lifecycle/lib/lifecycle_web/live/echo_live/index/index.ex
@@ -12,6 +12,7 @@ defmodule LifecycleWeb.EchoLive.Index do
 
   # TODO: separate msg by date
   # TODO: allow multiple images uploaded, currently multiple images name is concatenated with "##"
+  # TODO: modularize the code with functional components
 
   @impl true
   def mount(_params, _session, socket) do
@@ -135,23 +136,32 @@ defmodule LifecycleWeb.EchoLive.Index do
 
   def handle_event("approve", %{"value" => id}, socket) do
     echo = Timeline.get_echo!(id)
-    IO.inspect(echo)
+    IO.inspect(echo.transited)
 
-    echo_params = %{
-      transited: true,
-      transiter: socket.assigns.current_user.name
-    }
+    case echo.transited do
+      false -> {
+        echo_params = %{
+          transited: true,
+          transiter: socket.assigns.current_user.name
+        }}
 
-    case Timeline.update_transition(id, echo_params) do
-      {:ok, echo} ->
-        {Pubsub.notify_subs({:ok, echo}, [:echo, :created], "1")}
+        case Timeline.update_transition(id, echo_params) do
+          {:ok, echo} ->
+            {Pubsub.notify_subs({:ok, echo}, [:echo, :created], "1")}
 
-        {:noreply,
-         socket
-         |> put_flash(:info, "Transition approved!")}
+            {:noreply,
+             socket
+             |> put_flash(:info, "Transition approved!")}
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
+          {:error, %Ecto.Changeset{} = changeset} ->
+            {:noreply, assign(socket, :changeset, changeset)}
+        end
+
+      true -> {
+        :noreply,
+        socket
+        |> put_flash(:info, "Transition already approved!")
+      }
     end
   end
 
