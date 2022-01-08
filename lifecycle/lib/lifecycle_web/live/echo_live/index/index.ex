ddbe6a42d07e7ef6872d1d79ac4ea275727ff597
huSKY <jetxuen_lee@mymail.sutd.edu.sg>
2022-01-08 18:32:17 +0800

    image asset done with local file system

--- lifecycle/lib/lifecycle_web/live/echo_live/index/index.ex
+++ lifecycle/lib/lifecycle_web/live/echo_live/index/index.ex
@@ -11,6 +11,7 @@ defmodule LifecycleWeb.EchoLive.Index do
   alias Lifecycle.Timeline.Echo
 
   # TODO: separate msg by date
+  # TODO: allow multiple images uploaded, currently multiple images name is concatenated with "##"
 
   @impl true
   def mount(_params, _session, socket) do
@@ -19,7 +20,7 @@ defmodule LifecycleWeb.EchoLive.Index do
     timezone = socket.assigns.timezone
     timezone_offset = socket.assigns.timezone_offset
     changeset = Timeline.Echo.changeset(%Echo{})
-    socket = allow_upload(socket, :transition, accept: ~w(.jpg .jpeg), max_entries: 2)
+    socket = allow_upload(socket, :transition, accept: ~w(.png .jpg .jpeg), max_entries: 1)
 
     {:ok,
      assign(socket,
@@ -51,6 +52,7 @@ defmodule LifecycleWeb.EchoLive.Index do
 
   @impl true
   def handle_event("save", %{"echo" => echo_params}, socket) do
+
     case Timeline.create_echo(echo_params) do
       {:ok, echo} ->
         {Pubsub.notify_subs({:ok, echo}, [:echo, :created], "1")}
@@ -59,8 +61,6 @@ defmodule LifecycleWeb.EchoLive.Index do
           :noreply,
           socket
           |> put_flash(:info, "Message Sent")
-          #  |> Pubsub.notify_subs([:echo, :created], "1")
-          # pub sub to be added
         }
 
       {:error, %Ecto.Changeset{} = changeset} ->
@@ -82,27 +82,57 @@ defmodule LifecycleWeb.EchoLive.Index do
     |> Timezone.get_time(timezone, timezone_offset)
   end
 
-
   def handle_event("validate", _params, socket) do
     {:noreply, socket}
   end
-
 
   def handle_event("cancel-upload", %{"ref" => ref}, socket) do
     {:noreply, cancel_upload(socket, :transition, ref)}
   end
 
+  @doc """
+  Construct the file path of the image uploaded, and save it to local file system
+  Create transition echo object
+  """
   def handle_event("transit", _params, socket) do
+    # function to get the file path and save it to local file system
     uploaded_files =
-      consume_uploaded_entries(socket, :transition, fn %{path: path}, _entry ->
-        dest = Path.join([:code.priv_dir(:lifecycle), "static", "uploads", Path.basename(path)])
+      consume_uploaded_entries(socket, :transition, fn %{path: path}, entry ->
+        ext = entry.client_name
+        dest_path = path <> ext
+        # changes destination path name with extension for rendering
+        dest = Path.join([:code.priv_dir(:lifecycle), "static", "uploads", Path.basename(dest_path)])
         File.cp!(path, dest)
-        Routes.static_path(socket, "/#{Path.basename(dest)}")
+        Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
       end)
 
-    {:noreply, update(socket, :image_list, &(&1 ++ uploaded_files))}
-  end
+    # convert list to string
+    image_list = Enum.join(uploaded_files, "##")
 
+    # assign image path to socket
+    # socket = socket |> assign(image_list: file)
+
+    # construct echo_params for creating transition echo objects
+    echo_params = %{
+      "message" => image_list,
+      "type" => "transition",
+      "name" => socket.assigns.current_user.name
+    }
+
+    case Timeline.create_echo(echo_params) do
+      {:ok, echo} ->
+        {Pubsub.notify_subs({:ok, echo}, [:echo, :created], "1")}
+
+        {
+          :noreply,
+          socket
+          |> put_flash(:info, "Transition Object Sent")
+        }
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
 
   def error_to_string(:too_large), do: "Too large"
   def error_to_string(:too_many_files), do: "You have selected too many files"
