8aeddb656f1b1783128bbcd315bad09dfe989147
huSKY <jetxuen_lee@mymail.sutd.edu.sg>
2022-01-14 17:09:41 +0800

    function to get time in the format of 12 hrs

--- lifecycle/lib/lifecycle/timezone.ex
+++ lifecycle/lib/lifecycle/timezone.ex
@@ -33,11 +33,25 @@ defmodule Lifecycle.Timezone do
     |> assign_timezone_offset()
   end
 
-  def get_time(time, timezone, timezone_offset) do
+  @doc """
+    Get the date in the format of YYYY-MM-DD
+  """
+  def get_date(time, timezone, timezone_offset) do
     time
     |> DateTime.from_naive!(timezone)
     |> Timex.shift(hours: timezone_offset)
     |> Timex.format("{YYYY}-{0M}-{D}")
+    |> elem(1)
+  end
+
+  @doc """
+    Get the time in the format of 12hrs, e.x.: 5:06pm
+  """
+  def get_time(time, timezone, timezone_offset) do
+    time
+    |> DateTime.from_naive!(timezone)
+    |> Timex.shift(hours: timezone_offset)
+    |> Timex.format("{h12}:{m} {am}")
     |> elem(1)
   end
 end
--- lifecycle/lib/lifecycle_web/live/echo_live/index/index.ex
+++ lifecycle/lib/lifecycle_web/live/echo_live/index/index.ex
@@ -19,7 +19,6 @@ defmodule LifecycleWeb.EchoLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    IO.inspect("mount")
     if connected?(socket), do: Pubsub.subscribe("1")
     socket = Timezone.get_timezone(socket)
     timezone = socket.assigns.timezone
@@ -41,7 +40,6 @@ defmodule LifecycleWeb.EchoLive.Index do
 
   @impl true
   def handle_event("save", %{"echo" => echo_params}, socket) do
-    IO.inspect("handle_event create echo")
     case Timeline.create_echo(echo_params) do
       {:ok, echo} ->
         {Pubsub.notify_subs({:ok, echo}, [:echo, :created], "1")}
@@ -59,13 +57,11 @@ defmodule LifecycleWeb.EchoLive.Index do
 
   @impl true
   def handle_info({Pubsub, [:echo, :created], message}, socket) do
-    IO.inspect("handle_info echo created")
     {:noreply, assign(socket, :nowstream, [message | socket.assigns.nowstream])}
   end
 
   @impl true
   def handle_info({Pubsub, [:transition, :approved], message}, socket) do
-    IO.inspect("handle info transition approved")
     params = %{
       id: message.id,
       transiter: socket.assigns.current_user.name,
@@ -84,7 +80,6 @@ defmodule LifecycleWeb.EchoLive.Index do
          echo_stream: echo_stream,
          socket: socket
        }) do
-    # import IEx; IEx.pry();
     # pass back :ok, or :cont
     socket =
     socket
@@ -95,44 +90,25 @@ defmodule LifecycleWeb.EchoLive.Index do
         echo -> echo
       end)
     )
-
-    # {:noreply,
-    #  assign(
-    #    socket,
-    #    echo_stream,
-    #    Enum.map(socket.assigns[echo_stream], fn
-    #      %Echo{id: id} = echo -> %Echo{echo | transiter: transiter, transited: true}
-    #      echo -> echo
-    #    end)
-    #  )}
     {:noreply, socket}
   end
 
   defp list_echoes do
-    IO.inspect("list echoes")
     Timeline.recall()
-  end
-
-  def time_format(time, timezone, timezone_offset) do
-    time
-    |> Timezone.get_time(timezone, timezone_offset)
   end
 
   @doc """
     button event by transition button
   """
   def handle_event("transition", _params, socket) do
-    IO.inspect("handle event transition")
     Transition.handle_button("transition", socket)
   end
 
   def handle_event("validate", _params, socket) do
-    IO.inspect("validate")
     {:noreply, socket}
   end
 
   def handle_event("cancel-upload", %{"ref" => ref}, socket) do
-    IO.inspect("cancel upload")
     {:noreply, cancel_upload(socket, :transition, ref)}
   end
 
@@ -142,7 +118,6 @@ defmodule LifecycleWeb.EchoLive.Index do
   Create transition echo object
   """
   def handle_event("transit", _params, socket) do
-    IO.inspect("handle evetnt transit")
     # function to get the file path and save it to local file system
     uploaded_files =
       consume_uploaded_entries(socket, :transition, fn %{path: path}, entry ->
@@ -184,7 +159,6 @@ defmodule LifecycleWeb.EchoLive.Index do
   end
 
   def handle_event("approve", %{"value" => id}, socket) do
-    IO.inspect("approvef")
     Approve.handle_button(%{"value" => id}, socket)
   end
 
