6385822320b7f4f8b3da082354f7cd34ac18c9e1
huSKY <jetxuen_lee@mymail.sutd.edu.sg>
2022-01-16 19:18:21 +0800

    added moduledoc and doc for necessary modules and functions

--- lifecycle/lib/lifecycle_web/modal/button/transition.ex
+++ lifecycle/lib/lifecycle_web/modal/button/transition.ex
@@ -1,5 +1,7 @@
 defmodule LifecycleWeb.Modal.Button.Transition do
-
+  @moduledoc """
+  Transition button
+  """
   use Phoenix.Component
 
   def button(assigns) do
@@ -8,6 +10,10 @@ defmodule LifecycleWeb.Modal.Button.Transition do
     """
   end
 
+  @doc """
+  handle event for approve button
+  assign transiting to true, such that users can upload image to be approved
+  """
   def handle_button("transition", socket) do
     {:noreply,
      socket
