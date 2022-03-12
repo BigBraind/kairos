84ba7ae0dccb24a57aa9d96fa75e999887b763da
PohPrinceton <niceprinceton@gmail.com>
2022-03-12 23:31:45 +0800

    render transition list

--- lifecycle/lib/lifecycle_web/live/admin_live/view_transition.ex
+++ lifecycle/lib/lifecycle_web/live/admin_live/view_transition.ex
@@ -2,11 +2,14 @@ defmodule LifecycleWeb.AdminLive.ViewTransition do
   @moduledoc false
   use LifecycleWeb, :live_view
 
+  alias Lifecycle.Timeline
+  alias LifecycleWeb.Modal.View.Transition.TransitionList
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok,
-      socket
-    }
+     assign(socket,
+       transition_list: Timeline.list_transitions()
+     )}
   end
-
 end
