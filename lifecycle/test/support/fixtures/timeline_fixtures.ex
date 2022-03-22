174298dbf75df658a3ec4ea5b49758391328043f
joseph <josephchong1009@gmail.com>
2022-03-23 01:07:14 +0800

    added user to echo fixture

--- lifecycle/test/support/fixtures/timeline_fixtures.ex
+++ lifecycle/test/support/fixtures/timeline_fixtures.ex
@@ -5,6 +5,7 @@ defmodule Lifecycle.TimelineFixtures do
   """
 
   alias Lifecycle.Timeline
+  alias Lifecycle.Repo
 
   alias Ecto.UUID
 
@@ -13,13 +14,14 @@ defmodule Lifecycle.TimelineFixtures do
   """
   def echo_fixture(attrs \\ %{}) do
     phase = phase_fixture()
+    user = user_fixtures()
 
     {:ok, echo} =
       attrs
       |> Enum.into(%{
         phase_id: phase.id,
+        user_name: user.name,
         message: "some message",
-        name: "some name"
         # type: "type"
       })
       |> Timeline.create_echo()
@@ -42,5 +44,13 @@ defmodule Lifecycle.TimelineFixtures do
       |> Timeline.create_phase()
 
    %{phase | parent: [], child: []} # add child and parent
+  end
+
+  def user_fixtures(_attr \\ %{}) do
+    {:ok, user} =
+      %Lifecycle.Users.User{name: "Bruce Lee", id: Ecto.UUID.generate()}
+      |> Repo.insert()
+
+    user
   end
 end
