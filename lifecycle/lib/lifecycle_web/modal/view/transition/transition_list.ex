7714899c7d402d987d74b591365da419355a2b2b
Brandon Chong <bwandonchong@gmail.com>
2022-04-01 00:51:04 +0800

    transition cards done

--- lifecycle/lib/lifecycle_web/modal/view/transition/transition_list.ex
+++ lifecycle/lib/lifecycle_web/modal/view/transition/transition_list.ex
@@ -46,43 +46,8 @@ defmodule LifecycleWeb.Modal.View.Transition.TransitionList do
             <div class="max-w-2xl mx-auto">
 
                 <%= for transition <- @transitions do %>
-                <div id="default-carousel" class="relative" data-carousel="static">
-                <!-- Carousel wrapper -->
-                <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-                    <!-- Item 1 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <span class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-                    </div>
-                    <!-- Item 2 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-                    </div>
-                    <!-- Item 3 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-                    </div>
-                </div>
-                <!-- Slider indicators -->
-                <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                </div>
-                <!-- Slider controls -->
-                <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                        <span class="hidden">Previous</span>
-                    </span>
-                </button>
-                <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                        <span class="hidden">Next</span>
-                    </span>
-                </button>
-            </div>
+
+
 
                   <!-- first -->
 
@@ -101,8 +66,8 @@ defmodule LifecycleWeb.Modal.View.Transition.TransitionList do
                                   <source src={image_path} type={"audio/mp4"} >
                                   </audio>
                                 <% else %>
-                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                    <img src={image_path} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+                                <div class="active duration-700 ease-in-out" data-carousel-item>
+                                    <img src={image_path} class="block absolute top-1/2 left-1/2 w-full -translate-y-1/2 -translate-x-1/2 transition" alt="...">
                                 </div>
                                 <% end %>
                                 <% end %>
@@ -139,7 +104,7 @@ defmodule LifecycleWeb.Modal.View.Transition.TransitionList do
                     <% else %>
                         <button phx-click="transit", value={transition.id}>Approve?</button><br>
                     <% end %>
-
+                    <div class="flex items-center justify-center">
                     <%= if @id == "transition" do %>
                       <span><%= live_patch "Edit Transition", to: Routes.phase_show_path(@socket, :transition_edit, transition.phase_id, transition.id), class: "button" %></span>
                     <% else %>
@@ -147,9 +112,9 @@ defmodule LifecycleWeb.Modal.View.Transition.TransitionList do
                     <% end %>
 
                     <br>
-
-                    <span class = "px-4 py-1 text-lg bg-red-200 text-white font-light rounded-full hover:text-white hover:bg-red-600 hover:font-semibold"><%= link "Delete", to: "#", phx_click: "delete-transition", phx_value_id: transition.id, data: [confirm: "Are you sure?"], class: "button" %></span>
-                    <br>
+                      <span class = "px-4 py-1 text-lg bg-red-200 text-white font-light rounded-full hover:text-white hover:bg-red-600 hover:font-semibold"><%= link "Delete", to: "#", phx_click: "delete-transition", phx_value_id: transition.id, data: [confirm: "Are you sure?"], class: "button" %></span>
+                    </div>
+                      <br>
                     <br>
                 <% end %>
             </div>
