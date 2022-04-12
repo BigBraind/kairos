79b89475215f7d9470065d5e6ed0f70e5caaa907
huSKY <jetxuen_lee@mymail.sutd.edu.sg>
2022-04-12 21:46:41 +0800

    solve bugs

--- lifecycle/lib/lifecycle_web/modal/view/phases.ex
+++ lifecycle/lib/lifecycle_web/modal/view/phases.ex
@@ -13,16 +13,11 @@ defmodule LifecycleWeb.Modal.View.Transition.Phases do
     {:ok, assign(socket, phase: phase)}
   end
 
-  defdelegate phase_has_child?(phase_id), to: Timeline
-
   # TODO: Princeton: add any phase information here
   def render(assigns) do
     ~H"""
       <div>
           <h1><%= @phase.title %> </h1>
-          <%= if phase_has_child?(@phase.id) do %>
-            <button phx-click="display_child_phase" phx-value-phase_id={@phase.id} class="p-1 button flex bg-blue-500 text-white text-center font-semibold rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-75"> + </button>
-          <% end %>
       </div>
     """
   end
