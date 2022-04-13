cfe786fe6e634d736f0c0d23da33876430afb6ff
PohPrinceton <niceprinceton@gmail.com>
2022-04-14 02:30:23 +0800

    minimally working graph

---
+++ lifecycle/lib/lifecycle_web/live/realm_live/shared.ex
@@ -0,0 +1,103 @@
+defmodule ContexSampleWeb.Shared do
+  import Phoenix.HTML
+  import Phoenix.LiveView, only: [assign: 2]
+
+  def update_chart_options_from_params(socket, params) do
+    options =
+      socket.assigns.chart_options
+      |> clear_message()
+      |> update_if_int(:series, params["series"], 20)
+      |> update_if_int(:points, params["points"], 5000)
+      |> update_if_int(:categories, params["categories"], 40)
+      |> update_type(params["type"])
+      |> update_orientation(params["orientation"])
+      |> Map.put(:show_legend, params["show_legend"])
+      |> Map.put(:show_axislabels, params["show_axislabels"])
+      |> Map.put(:show_data_labels, params["show_data_labels"])
+      |> Map.put(:show_selected, params["show_selected"])
+      |> Map.put(:smoothed, params["smoothed"])
+      |> Map.put(:custom_value_scale, params["custom_value_scale"])
+      |> Map.put(:custom_x_scale, params["custom_x_scale"])
+      |> Map.put(:custom_y_scale, params["custom_y_scale"])
+      |> Map.put(:custom_y_ticks, params["custom_y_ticks"])
+      |> Map.put(:time_series, params["time_series"])
+      |> Map.put(:title, params["title"])
+      |> Map.put(:subtitle, params["subtitle"])
+      |> Map.put(:colour_scheme, params["colour_scheme"])
+
+     assign(socket, chart_options: options)
+  end
+
+  def colour_options(), do: simple_option_list(~w(default themed custom warm pastel nil))
+  def yes_no_options(), do: simple_option_list(~w(yes no))
+  def chart_type_options(), do: simple_option_list(~w(stacked grouped))
+  def chart_orientation_options(), do: simple_option_list(~w(vertical horizontal))
+
+
+  def lookup_colours("pastel"), do: :pastel1
+  def lookup_colours("default"), do: :default
+  def lookup_colours("warm"), do: :warm
+  def lookup_colours("themed"), do: ["ff9838", "fdae53", "fbc26f", "fad48e", "fbe5af", "fff5d1"]
+  def lookup_colours("custom"), do: ["004c6d", "1e6181", "347696", "498caa", "5da3bf", "72bbd4", "88d3ea", "9eebff"]
+  def lookup_colours("nil"), do: nil
+  def lookup_colours(_), do: nil
+
+  def simple_option_list(options), do: Enum.map(options, &%{name: &1, value: &1})
+
+  def raw_select(name, id, options, current_item) do
+    beginning_bit = ~E|<select  type="select" name="<%= name %>" id="<%= id %>">|
+
+    middle_bit = Enum.map(options, fn o ->
+      selected = if o.value == current_item, do: "selected", else: ""
+      ~E|<option value="<%= o.value %>" <%= selected %>><%= o.name %></option>|
+    end)
+
+    end_bit = ~E|</select>|
+    [beginning_bit, middle_bit, end_bit]
+  end
+
+  def list_to_comma_string(nil), do: ""
+  def list_to_comma_string(list), do: Enum.join(list, ", ")
+
+  defp clear_message(map), do: Map.put(map, :friendly_message, [])
+
+  defp add_message(map, msg) do
+    existing = case map[:friendly_message] do
+      [] -> ["Are you trying to crash the demo server?!. Feel free to clone the repo if you want to test the limits. The BEAM generally keeps up - browser rendering is usually where things drag."]
+      [_|_] = val -> val
+    end
+    Map.put(map, :friendly_message, [msg | existing])
+  end
+
+  defp update_if_int(map, _key, nil, _max), do: map
+  defp update_if_int(map, key, possible_value, max) do
+    case Integer.parse(possible_value) do
+      {val, ""} ->
+        if val > max do
+          Map.put(map, key, max)
+          |> add_message("Maximum for #{key} is #{max}")
+        else
+          if val > 0, do: Map.put(map, key, val), else: map
+        end
+      _ ->
+        map
+    end
+  end
+
+  defp update_type(options, raw) do
+    case raw do
+      "stacked" -> Map.put(options, :type, :stacked)
+      "grouped" -> Map.put(options, :type, :grouped)
+      _-> Map.put(options, :type, raw)
+    end
+  end
+
+  defp update_orientation(options, raw) do
+    case raw do
+      "horizontal" -> Map.put(options, :orientation, :horizontal)
+      "vertical" -> Map.put(options, :orientation, :vertical)
+      _-> options
+    end
+  end
+
+end
--- lifecycle/lib/lifecycle_web/live/realm_live/show.ex
+++ lifecycle/lib/lifecycle_web/live/realm_live/show.ex
@@ -1,10 +1,30 @@
 defmodule LifecycleWeb.RealmLive.Show do
   use LifecycleWeb, :live_view
 
+  import ContexSampleWeb.Shared
+
+  alias Contex.{LinePlot, PointPlot, Dataset, Plot}
   alias Lifecycle.Users
 
   @impl true
   def mount(%{"party_name" => party, "realm_name" => realm}, _session, socket) do
+    socket =
+      socket
+      |> assign(chart_options: %{
+          series: 4,
+          points: 30,
+          title: nil,
+          type: "line",
+          smoothed: "yes",
+          colour_scheme: "default",
+          show_legend: "yes",
+          custom_x_scale: "no",
+          custom_y_scale: "no",
+          custom_y_ticks: "no",
+          time_series: "no"
+          })
+      |> assign(prev_series: 0, prev_points: 0, prev_time_series: nil)
+      |> make_test_data()
     realm_topic = "realm:" <> party <> ":" <> realm
     if connected?(socket), do: Lifecycle.Pubsub.subscribe(realm_topic)
     {:ok,
@@ -43,4 +63,112 @@ defmodule LifecycleWeb.RealmLive.Show do
 
   defp page_title(:show), do: "Show Realm"
   defp page_title(:edit), do: "Edit Realm"
+
+  def build_pointplot(dataset, chart_options) do
+    y_tick_formatter = case chart_options.custom_y_ticks do
+      "yes" -> &custom_axis_formatter/1
+      _ -> nil
+    end
+
+    module = case chart_options.type do
+      "line" -> LinePlot
+      _ -> PointPlot
+    end
+
+    custom_x_scale = make_custom_x_scale(chart_options)
+    custom_y_scale = make_custom_y_scale(chart_options)
+
+    options = [
+      mapping: %{x_col: "X", y_cols: chart_options.series_columns},
+      colour_palette: lookup_colours(chart_options.colour_scheme),
+      custom_x_scale: custom_x_scale,
+      custom_y_scale: custom_y_scale,
+      custom_y_formatter: y_tick_formatter,
+      smoothed: (chart_options.smoothed == "yes")
+    ]
+
+    plot_options = case chart_options.show_legend do
+      "yes" -> %{legend_setting: :legend_right}
+      _ -> %{}
+    end
+
+
+    plot = Plot.new(dataset, module, 600, 400, options)
+      |> Plot.titles(chart_options.title, nil)
+      |> Plot.plot_options(plot_options)
+
+    Plot.to_svg(plot)
+  end
+
+  defp make_test_data(socket) do
+    options = socket.assigns.chart_options
+    time_series = (options.time_series == "yes")
+    prev_series = socket.assigns.prev_series
+    prev_points = socket.assigns.prev_points
+    prev_time_series = socket.assigns.prev_time_series
+    series = options.series
+    points = options.points
+
+    needs_update = (prev_series != series) or (prev_points != points) or (prev_time_series != time_series)
+
+    # TODO: insert data here
+    data = [
+      [1, 135, 231, 1233, 213],
+      [2, 432, 21, 321, 234],
+      [3, 135, 423, 121, 534],
+      [4, 135, 21, 1112, 1233],
+      [5, 135, 231, 1233, 213],
+      [6, 432, 21, 321, 234],
+      [7, 135, 423, 121, 534],
+      [8, 135, 21, 1112, 1233],
+    ]
+
+    # TODO: add the lable for the data here
+    series_cols = ["a","b","c","d"]
+
+    test_data = case needs_update do
+      true ->  Dataset.new(data, ["X" | series_cols])
+      _ -> socket.assigns.test_data
+    end
+
+    options = Map.put(options, :series_columns, series_cols)
+
+    assign(socket,
+      test_data: test_data,
+      chart_options: options,
+      prev_series: series,
+      prev_points: points,
+      prev_time_series: time_series
+    )
+  end
+
+  def custom_axis_formatter(value) when is_float(value) do
+    "V #{:erlang.float_to_binary(value/1_000.0, [decimals: 2])}K"
+  end
+
+  def custom_axis_formatter(value) do
+    "V #{value}"
+  end
+
+  defp make_custom_x_scale(%{custom_x_scale: x}=_chart_options) when x != "yes", do: nil
+  defp make_custom_x_scale(chart_options) do
+    case (chart_options.time_series == "yes") do
+      true ->
+        Contex.TimeScale.new()
+
+      _ ->
+        Contex.ContinuousLinearScale.new()
+        |> Contex.ContinuousLinearScale.domain(0, 100)
+        |> Contex.ContinuousLinearScale.interval_count(20)
+
+    end
+  end
+
+  defp make_custom_y_scale(%{custom_y_scale: x}=_chart_options) when x != "yes", do: nil
+  defp make_custom_y_scale(_chart_options) do
+    Contex.ContinuousLinearScale.new()
+    |> Contex.ContinuousLinearScale.domain(0, 100)
+    |> Contex.ContinuousLinearScale.interval_count(20)
+  end
+
 end
