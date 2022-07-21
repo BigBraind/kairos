621c3ff5b9262184e7947a489cc00ff6e7de6d3d
necr0z <necroz1998@gmail.com>
2022-07-22 05:13:44 +0800

    can see some things roflmao

--- lifecycle/lib/lifecycle_web/live/journey_live/show.ex
+++ lifecycle/lib/lifecycle_web/live/journey_live/show.ex
@@ -4,11 +4,70 @@ defmodule LifecycleWeb.JourneyLive.Show do
   alias Lifecycle.Realm
 
   @impl true
-  def mount(params, _session, socket) do
-    {:ok,
-    socket |> assign(:journeys, list_journeys())
-  }
+  def mount(%{"id" => id} = _params, _session, socket) do
+    case Realm.get_journey!(id) do
+      {:ok, journey} -> {:ok, assign(socket, journey: journey, traits_form: [], changeset: Ecto.Changeset.change(journey))}
+      _ -> {:ok, assign(socket, :journey, nil)}
+    end
+
   end
+
+  def update_realm_name(resource, %{"journey" => %{"value" => realm_name}}) do
+    Realm.update_journey(resource, %{realm_name: realm_name})
+  end
+
+  @impl true
+  def handle_event("take-down", _params, %{assigns: %{journey: journey}} = socket) do
+    case Realm.update_journey(journey, %{active: false}) do
+      {:ok, journey} ->
+        {:noreply, socket
+        |> assign(journey: journey)
+        |> put_flash(:info, "journey status updated.")}
+      _ ->
+        {:noreply, socket
+        |> put_flash(:error, "journey status failed to update")}
+    end
+  end
+
+  @impl true
+  def handle_event("trait_management", %{"method" => "delete", "id" => id} = params, %{assigns: %{traits_form: val}} = socket) do
+    index = String.to_integer(id)
+    {:noreply, assign(socket, [traits_form: List.delete_at(val, index)])}
+  end
+
+  @impl true
+  def handle_event("trait_management", %{"method" => "add"}, %{assigns: %{traits_form: val}} = socket) do
+    {:noreply, assign(socket, [traits_form: val ++ [""]])}
+  end
+
+  @impl true
+  def handle_event("trait_management", _params, socket) do
+    {:noreply, assign(socket, [traits_form: [""]])}
+  end
+
+  @impl true
+  def handle_event("trait_change", %{"orb" => trait_change} = _params, socket) do
+    traits = Map.values(trait_change)
+    {:noreply, assign(socket, :traits_form, traits)}
+  end
+
+
+  @impl true
+  def handle_event("save_trait", %{"journey" => trait_change} = _params, %{assigns: %{journey: journey}} = socket) do
+    traits = Map.values(trait_change)
+    case Realm.update_journey(journey, %{traits: journey.traits ++ traits}) do
+      {:ok, journey} ->
+        {:noreply, socket
+        |> assign([journey: journey, traits_form: [], changeset: Ecto.Changeset.change(journey)])
+        |> put_flash(:info, "journey traits updated.")}
+      _ ->
+        {:noreply, socket
+        |> put_flash(:error, "journey traits failed to update")}
+    end
+  end
+
+
+
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
